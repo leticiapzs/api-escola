05408bc755fe279354a41cd2cb2e9b756538ba56
Letícia Peixoto <leticia_95lima@hotmail.com>
2025-01-05 12:34:32 -0300

    feat: criação dos models, views, serializers e urls da matricula

--- ESCOLA/escola/serializers.py
+++ ESCOLA/escola/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
-from escola.models import Estudante,Curso
+from escola.models import Estudante,Curso,Matricula
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
-        fields = ['id','nome','email','cpf','data_nasciemento','celular']
+        fields = ['id','nome','email','cpf','data_nascimento','celular']
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'        
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        fields = '__all__' 
+
             
--- ESCOLA/setup/urls.py
+++ ESCOLA/setup/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import estudantes
+from django.urls import path,include
+from escola.views import EstudanteViewSet,CursoViewSet,MatriculaViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('estudante',EstudanteViewSet,basename='Estudantes')
+router.register('cursos',CursoViewSet,basename='Cursos')
+router.register('matriculas',MatriculaViewSet,basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estudantes/',estudantes)
+    path('',include(router.urls))
 ]
