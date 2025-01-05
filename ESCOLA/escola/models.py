f4af535c86daeff5795525217b7ee4526965effc
Letícia Peixoto <leticia_95lima@hotmail.com>
2025-01-04 21:21:31 -0300

    feat: criação do admin e serializers

--- ESCOLA/escola/models.py
+++ ESCOLA/escola/models.py
@@ -10,7 +10,7 @@ class Estudante(models.Model):
     def __str__(self):
         return self.nome
     
-    class Curso(models.Model):
+class Curso(models.Model):
         NIVEL = (
             ('B', 'Básico'),
             ('I', 'Intermediário'),
